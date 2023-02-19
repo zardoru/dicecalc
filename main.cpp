0992232ddcd45b6509e303289ae7a22d4e3c8cbe
zardoru <diao.lzr@gmail.com>
2023-02-19 16:49:57 -0300

    use cpp_bin_float_oct instead of double.

--- main.cpp
+++ main.cpp
@@ -3,29 +3,44 @@
 #include <boost/math/special_functions/binomial.hpp>
 #include <boost/json.hpp>
 #include <boost/json/object.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
 #include <omp.h>
+
+#define TOLERANCE 1E-5
 
 namespace bm = boost::math;
 namespace b = boost;
 namespace json = boost::json;
 
-using diceInt = double;
+using diceInt = boost::multiprecision::cpp_bin_float_oct;
 
 json::object run_simulation(double dicesides, double dicecount);
 
-double chance(diceInt points, diceInt dice, diceInt sides) {
-    diceInt sum = 0;
+/* https://stackoverflow.com/questions/22685563/integer-calculation-of-the-binomial-coefficient-using-boostmathbinomial-coef */
+diceInt BinomialCoefficient(const diceInt& n, const diceInt& k) {
+    if (k == 0) { return 1; }
+    else { return (n * BinomialCoefficient(n - 1, k - 1)) / k; }
+}
+
+diceInt chance(const diceInt& points, const diceInt& dice, const diceInt& sides) {
     const diceInt lim = floor((points - dice) / sides);
 
-    #pragma omp parallel for reduction(+:sum)
+    std::vector<diceInt> sumVec((int)lim + 1);
+
+    #pragma omp parallel for
     for (int i = 0; i <= (int)lim; ++i) {
-        auto sign = pow(-1, i);
-        auto c2 = bm::binomial_coefficient<diceInt>(points - sides * i - 1, dice - 1);
-        auto c1 = bm::binomial_coefficient<diceInt>(dice, i);
-        sum += sign * c1 * c2;
+        const auto sign = pow(-1, i);
+        const auto c2 = BinomialCoefficient(points - sides * i - 1, dice - 1);
+        const auto c1 = BinomialCoefficient(dice, i);
+        sumVec[i] = sign * c1 * c2;
     }
 
-    return double (sum) / double (pow(sides, dice));
+    diceInt sum = 0;
+    for (int i = 0; i <= (int)lim; ++i) {
+        sum += sumVec[i];
+    }
+
+    return diceInt (sum) / diceInt (pow(sides, dice));
 }
 
 
@@ -38,26 +53,40 @@ int main(int argc, char* argv[]) {
     auto dicesides = b::lexical_cast<double>(argv[2]);
     auto dicecount = b::lexical_cast<double>(argv[1]);
 
+    bool single_sim = false;
+
+    if (argc > 2)
+        single_sim = true;
+
+    std::cerr << "num type size: " << sizeof (diceInt) << std::endl;
+
     json::object output;
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    #pragma omp parallel
-    {
-        #pragma omp single
+    if (!single_sim) {
+
+#pragma omp parallel
         {
-            for (int i = 1; i <= dicecount; i++) {
-                #pragma omp task firstprivate(i)
-                {
-                    char line[1024];
-                    snprintf(line, sizeof line, "simulating %dd%.0f (thread: %d)\n", i, dicesides, omp_get_thread_num());
-                    std::cerr << line;
-                    auto item = run_simulation(dicesides, i);
-                    #pragma omp critical
-                    output[b::lexical_cast<std::string>(i)] = item;
+#pragma omp single
+            {
+                for (int i = 1; i <= dicecount; i++) {
+#pragma omp task firstprivate(i)
+                    {
+                        char line[1024];
+                        snprintf(line, sizeof line, "simulating %dd%.0f (thread: %d)\n", i, dicesides,
+                                 omp_get_thread_num());
+                        std::cerr << line;
+                        auto item = run_simulation(dicesides, i);
+#pragma omp critical
+                        output[b::lexical_cast<std::string>(i)] = item;
+                    }
                 }
             }
         }
+    } else {
+        auto item = run_simulation(dicesides, dicecount);
+        output[b::lexical_cast<std::string>(dicecount)] = item;
     }
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -69,20 +98,38 @@ int main(int argc, char* argv[]) {
 }
 
 json::object run_simulation(double dicesides, double dicecount) {
-    auto minroll = dicecount;
-    auto maxroll = dicesides * dicecount;
+    const auto minroll = (int)dicecount;
+    const auto maxroll = (int) (dicesides * dicecount);
+    std::vector<diceInt> probVec(maxroll - minroll + 1);
+
+    // #pragma omp parallel for
+    for (int l = minroll; l <= maxroll; l++) {
+        const auto prob = chance(l, dicecount, dicesides) * 100;
+        probVec[l - minroll] = prob;
+
+        if (prob < 0 || prob > 100) {
+            diceInt clampVal;
+
+            if (prob < 0)
+                clampVal = - prob;
+            else
+                clampVal = prob - 100;
+
+            if (clampVal > TOLERANCE)
+                std::cerr << "bad probability at xdx = " << l << " of " << prob << "%. Error is " << clampVal << "\n";
+        }
+    }
 
     json::object object;
-    std::cout.precision(std::numeric_limits<double>::max_digits10);
 
-    double sum = 0;
+    diceInt sum = 0;
     for (int l = minroll; l <= maxroll; ++l) {
-        auto prob = chance(l, dicecount, dicesides) * 100;
-        sum += prob;
+        auto prob = probVec[l - minroll];
 
+        sum += prob;
         json::object item = {
-                {"prob", prob},
-                {"sum", sum}
+                {"prob", (double) prob},
+                {"sum",  (double) sum}
         };
 
         object[b::lexical_cast<std::string>(l)] = item;
